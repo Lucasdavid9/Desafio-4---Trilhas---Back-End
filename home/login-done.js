1039367d84dfa83650322705bfc95f8785a5e673
Lucas David <lucasmartinsjesus9@gmail.com>
2025-05-24 00:36:11 -0300

    atualização

--- home/login-done.js
+++ home/login-done.js
@@ -7,12 +7,6 @@ authArea.innerHTML = `
     <span class="user-name">Olá, ${usuario.nome}</span>
     <button onclick="logout()" class="logout">Sair</button>
 `;
-
-convite.innerHTML = `
-    <h2>Bem-vindo de volta!</h2>
-    <p>Explore o conteúdo e participe ativamente da nossa comunidade.</p>
-    <a href="/denuncias.html" class="btn cadastre-agora">Fazer denúncia</a>
-`;
 }
 
 function logout() {
